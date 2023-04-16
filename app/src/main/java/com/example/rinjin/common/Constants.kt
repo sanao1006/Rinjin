50b23d81a48e47c4d3c1c6a41dfb4027df54edfc
sanao1006 <naosak1006@gmail.com>
2023-04-16 21:42:47 +0900

    fix: typo #25

--- app/src/main/java/com/example/rinjin/common/Constants.kt
+++ app/src/main/java/com/example/rinjin/common/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.rinjin.common
 
-import com.example.todofuken.BuildConfig
+import com.example.rinjin.BuildConfig
 
 object Constants {
     const val API_TOKEN = BuildConfig.API_TOKEN

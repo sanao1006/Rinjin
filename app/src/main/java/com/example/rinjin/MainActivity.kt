114f6ae1b1049a9b8c91dc845e4b236fe599c149
sanao1006 <naosak1006@gmail.com>
2023-04-23 18:45:19 +0900

    design: 入力した`location`から結果を表示させるようにした

--- app/src/main/java/com/example/rinjin/MainActivity.kt
+++ app/src/main/java/com/example/rinjin/MainActivity.kt
@@ -7,20 +7,28 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.rinjin.presentation.UsersViewModel
 import com.example.rinjin.ui.theme.RinjinTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -41,21 +49,46 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
-fun Rinjin(modifier: Modifier = Modifier) {
+fun Rinjin(
+    modifier: Modifier = Modifier,
+    viewModel: UsersViewModel = hiltViewModel()
+) {
+    val nowLocation by viewModel.locationInputText.collectAsState()
+    val keyboardController = LocalSoftwareKeyboardController.current
     Box(
         modifier =
-        modifier.fillMaxSize().padding(16.dp).background(color = Color.White, shape = ShapeDefaults.Small),
+        modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(color = Color.White, shape = ShapeDefaults.Small),
         contentAlignment = Alignment.TopCenter,
     ) {
-        OutlinedTextField(
-            modifier = Modifier.padding(top = 16.dp),
-            value = "",
-            onValueChange = {},
-            singleLine = true,
-            label = { Text(text = "Input Location!") },
-            trailingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "") },
-        )
+        Box(contentAlignment = Alignment.TopCenter) {
+            OutlinedTextField(
+                modifier = Modifier.padding(top = 16.dp),
+                value = nowLocation,
+                onValueChange = { viewModel.changeLocation(it) },
+                label = { Text(text = "Input Location!") },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    viewModel.getUsers("location:$nowLocation")
+                    keyboardController?.hide()
+                }),
+            )
+        }
+
+        if (nowLocation.isNotEmpty()) {
+            LazyColumn(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 16.dp),
+            ) {
+                items(viewModel.usersState.value.users) {
+                    Text(text = "${it.name} ${it.follower_val}", color = Color.Black)
+                }
+            }
+        }
     }
 }

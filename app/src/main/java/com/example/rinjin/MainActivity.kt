c77512646c4de9b69de672dadf936fc1e90b070b
sanao1006 <naosak1006@gmail.com>
2023-04-25 14:03:28 +0900

    design: Implementation of search results UI #31

--- app/src/main/java/com/example/rinjin/MainActivity.kt
+++ app/src/main/java/com/example/rinjin/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.ShapeDefaults
@@ -23,11 +33,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.example.rinjin.presentation.UsersViewModel
 import com.example.rinjin.ui.theme.RinjinTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -65,7 +77,10 @@ fun Rinjin(
             .background(color = Color.White, shape = ShapeDefaults.Small),
         contentAlignment = Alignment.TopCenter,
     ) {
-        Box(contentAlignment = Alignment.TopCenter) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
             OutlinedTextField(
                 modifier = Modifier.padding(top = 16.dp),
                 value = nowLocation,
@@ -77,16 +92,43 @@ fun Rinjin(
                     keyboardController?.hide()
                 }),
             )
-        }
-
-        if (nowLocation.isNotEmpty()) {
-            LazyColumn(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 16.dp),
-            ) {
-                items(viewModel.usersState.value.users) {
-                    Text(text = "${it.name} ${it.follower_val}", color = Color.Black)
+            if (nowLocation.isNotEmpty()) {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(bottom = 16.dp, top = 16.dp),
+                ) {
+                    items(viewModel.usersState.value.users) {
+                        Card(
+                            modifier = Modifier
+                                .padding(bottom = 16.dp, start = 16.dp, end = 16.dp)
+                                .fillMaxSize(),
+                        ) {
+                            Row(Modifier.padding(16.dp)) {
+                                AsyncImage(
+                                    model = it.avatar_url,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .size(64.dp)
+                                        .clip(CircleShape),
+                                )
+                                Column(
+                                    modifier = Modifier
+                                        .padding(start = 32.dp, top = 8.dp)
+                                        .fillMaxSize(),
+                                    verticalArrangement = Arrangement.Center,
+                                ) {
+                                    Row(modifier = Modifier.align(Alignment.Start)) {
+                                        Icon(
+                                            imageVector = Icons.Default.Person,
+                                            contentDescription = null,
+                                        )
+                                        Text(text = "${it.follower_val}", color = Color.Black)
+                                    }
+                                    Text(text = it.name, modifier = Modifier.padding(top = 2.dp))
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
